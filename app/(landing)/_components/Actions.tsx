85b9901d67e93a664e9b7edfb3b5f311abc22e75
Shakitskiy Vladislav <Vshakitskiy@gmail.com>
2024-01-31 21:13:09 +0300

    fix: csr

--- app/(landing)/_components/Actions.tsx
+++ app/(landing)/_components/Actions.tsx
@@ -7,13 +7,41 @@ import { MenuIcon } from "lucide-react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
 import { Session } from "next-auth"
 import { signOut } from "next-auth/react"
+import { Loader2 as Spinner } from "lucide-react"
+import { useEffect, useState } from "react"
 
 const Actions = ({ session }: { 
     session: Session | null 
 }) => {
-    const width = useMedia(500)
-    console.log(width)
-    return (
+    
+    const [width, setWidth] = useState({
+        width: 0,
+        isMobile: false,
+        client: false
+    })
+
+    useEffect(() => {
+        setWidth({
+            width: window.innerWidth,
+            isMobile: window.innerWidth < 500,
+            client: true
+        })
+    }, [])
+
+    useEffect(() => {
+        const resizeHandler = () => 
+            setWidth({
+                ...width,
+                width: window.innerWidth,
+                isMobile: window.innerWidth < 500,
+            })
+
+        window.addEventListener("resize", resizeHandler)
+        return () =>
+            window.removeEventListener("resize", resizeHandler)
+    }, [width])
+
+    return width.client ? (
         <>
             {width.isMobile ? (
                 <div>
@@ -80,6 +108,10 @@ const Actions = ({ session }: {
                     )}
                 </div>
             )}
+        </>
+    ) : (
+        <>
+            <Spinner className="ml-1 h-6 w-6 animate-spin" />
         </>
     )
 }
